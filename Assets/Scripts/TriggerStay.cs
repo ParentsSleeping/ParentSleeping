da57bb5561898a899cba59597402b48eb482b212
netanelbenis <netnel.b.i@gmail.com>
2020-06-22 00:01:56 +0300

    add a volume bar

---
+++ Assets/Scripts/TriggerStay.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerStay : MonoBehaviour
+{
+   public bool isDamaging;
+
+
+   private void OnTriggerStay(Collider other) {       
+       if(other.tag=="Player"&&this.tag=="loaf"){
+           other.SendMessage("takeNoise",Time.deltaTime*30);
+       }
+      
+   }
+   private void OnTriggerExit(Collider other) {
+       if(other.tag=="Player"){
+           other.SendMessage("healNoise",Time.deltaTime*30);
+       }
+   }
+}
---
+++ Assets/Scripts/VolumeBar.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VolumeBar : MonoBehaviour
+{
+public Image currentVolumeBar;
+public Text ratioText;
+private float hitPoint=0;
+private float maxHitPoint=150;
+private bool isTouching=false;
+
+private void Start() {
+    UpdateVolumeBar();
+}
+
+private void Update() {
+    if(!isTouching){
+        healNoise(Time.deltaTime*30);
+    }
+}
+
+private void UpdateVolumeBar() {
+    float ratio=hitPoint/maxHitPoint;
+    currentVolumeBar.rectTransform.localScale=new Vector3(ratio,1,1);
+    ratioText.text=(ratio*100).ToString()+'%';
+}
+
+private void healNoise(float noise){
+    isTouching=false;
+    hitPoint-=noise;
+    if(hitPoint<0){
+        hitPoint=0;
+    }
+    UpdateVolumeBar();
+
+}
+
+private void takeNoise(float silence){
+    isTouching=true;
+    hitPoint+=silence;
+    if(hitPoint>maxHitPoint){
+        hitPoint=maxHitPoint;
+    }
+        UpdateVolumeBar();
+
+}
+ 
+}
